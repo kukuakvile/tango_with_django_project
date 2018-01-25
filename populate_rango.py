8c9639d4a080f221aa17bf5b1a57c6874aa92798
Akvile Kurilaite <1106148k@student.gla.ac.uk>
2018-01-25 16:37:41 +0000

    chapter 5 mid ex

--- populate_rango.py
+++ populate_rango.py
@@ -52,8 +52,10 @@ def add_page(cat, title, url, views=0):
         p.save()
         return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
         c = Category.objects.get_or_create(name=name)[0]
+        c.views=views
+        c.likes=likes
         c.save()
         return c
 
